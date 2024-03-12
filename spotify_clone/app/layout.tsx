fd3026b0b55db12058f82691278a17f13fa131ae
Aryan-chand <imaryan1803@gmail.com>
2024-03-12 23:07:05 +0530

    layout

---
+++ spotify_clone/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Figtree } from "next/font/google";
+import "./globals.css";
+
+const font = Figtree({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Spotify",
+  description: "Listen to music",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        {children}
+      </body>
+    </html>
+  );
+}
