880f84594b728287d896807844be64863ef2d80c
Aryan-chand <imaryan1803@gmail.com>
2024-03-14 23:24:34 +0530

    ...

--- spotify_clone/component/sidebar.tsx
+++ spotify_clone/component/sidebar.tsx
@@ -1,3 +1,10 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Box from "./box";
 interface SidebarProps {
     children: React.ReactNode;
 }
@@ -5,9 +12,41 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> =({
     children
 }) => {
+    const pathname = usePathname();
+    
+    const routes = useMemo(() => [
+        {
+            icon : HiHome,
+            label: "Home",
+            active: pathname !== '/search',
+            href : '/',
+        },
+        {
+            icon : BiSearch,
+            label: "Search",
+            active: pathname === '/search',
+            href : '/' ,
+        }
+    ] ,[pathname]);
+
     return (
-        <div>
-            {children}
+        <div className="flex h-full">
+            <div 
+                className="
+                    hidden 
+                    md:flexflex
+                    flex-col
+                    gap-y-2
+                    bg-black
+                    h-full
+                    w-[300px]
+                    p-2
+                    "
+            >
+                <Box>
+                    Sidebar Navigation
+                </Box>
+            </div>
         </div>
         );
 }
